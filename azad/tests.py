afb36fb2323a7fcf1e8b3d6264824ab247ac4955
Arif Moazy <arifmoazy351@gmail.com>
2023-11-07 00:06:08 +0100

    update the test.py file7

--- azad/tests.py
+++ azad/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 class TryDjangoConfigTest(TestCase):
      def test_the_config(self):
           a = 1
-          b = 1
+          b = 2
           self.assertTrue(a==b)
 
 
